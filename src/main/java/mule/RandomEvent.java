3fdd2a7a52053d1e929f284ff14cf557c88706d1
evanyui <evanyui@hotmail.com>
2015-10-22 17:31:20 -0400

    Implement random event

--- src/main/java/mule/RandomEvent.java
+++ src/main/java/mule/RandomEvent.java
@@ -2,35 +2,52 @@ package mule;
 
 import mule.player.Player;
 
-import java.util.concurrent.Callable;
+import java.util.function.Consumer;
+import java.util.function.Function;
 
 public enum RandomEvent {
-    ALUMNI_PACKAGE((Player player) -> {
+    ALUMNI_PACKAGE((Player player) -> (Integer m) -> {
         //Add 3 food and 2 energy
         player.getFoodProp().add(3);
         player.getEnergyProp().add(2);
     }),
-    WANDERING_STUDENT((Player player) -> {
+    WANDERING_STUDENT((Player player) -> (Integer m) -> {
         //Add 2 ore
         player.getOreProp().add(2);
     }),
-    MUSEUM_PURCHASE((Player player) -> {
+    MUSEUM_PURCHASE((Player player) -> (Integer m) -> {
         //Add 8 * m (from current round)
+        player.addMoney(8*m);
+    }),
+    DEAD_RAT((Player player) -> (Integer m) -> {
+        //Get 2*m money
+        player.addMoney(2*m);
+    }),
+    FLYING_CATBUGS((Player player) -> (Integer m) -> {
+        //Lose 4*m money
+        player.debitMoney(4*m);
+    }),
+    UGA_STUDENT((Player player) -> (Integer m) -> {
+        player.getFoodProp().set(player.getFood() / 2);
+    }),
+    SPACE_GYPSY((Player player) -> (Integer m) -> {
+        player.debitMoney(6*m);
+    });
 
-    })
-    ;
+    private final Function<Player, Consumer<Integer>> event;
 
-    Player player;
-    PlayerCallable event;
+    private static final int[] roundFactor = {
+        25, 25, 25, 50, 50, 50, 50, 75, 75, 75, 75, 100
+    };
 
-    RandomEvent(PlayerCallable event) {
+    RandomEvent(Function<Player, Consumer<Integer>> event) {
         this.event = event;
-        this.player = null;
     }
 
-    public void runEvent(Player player) {
+    public void runEvent(Player player, int round) {
+        int m = (round < 13 && round > 0) ? roundFactor[round - 1] : 0;
         try {
-            event.call(player);
+            event.apply(player).accept(m);
         } catch (Exception e) {
             e.printStackTrace();
         }
