cc77dba4f470922ebeae1b5a8c5a2a34865d9a5e
Kelvin Chen <kelvin@kelvinchen.org>
2015-10-29 21:52:29 -0400

    Implement method to restore a loaded game
    I haven't really tested it so I'm not sure if this works. The method
    restores a game using the following saved classes:
    - GameState
    - TurnManager
    - Map
    
    I believe that these classes contain all the unique game data. There may
    be some stuff that I have missed, but the vast majority of game data
    should be loaded.

--- src/main/java/mule/configscreen/loadscreen/LoadScreenPresenter.java
+++ src/main/java/mule/configscreen/loadscreen/LoadScreenPresenter.java
@@ -1,30 +1,34 @@
 package mule.configscreen.loadscreen;
 
+import com.airhacks.afterburner.injection.Injector;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.EventType;
 import javafx.fxml.FXML;
-import javafx.scene.control.TableColumn;
+import javafx.scene.Scene;
 import javafx.scene.control.TableView;
-import javafx.scene.text.Text;
-import mule.Database;
-import mule.GameState;
+import javafx.stage.Stage;
+import mule.*;
+import mule.mainscreen.MainScreenView;
 import mule.player.Player;
+import mule.world.map.Map;
 import mvp.Presenter;
-import mvp.Validateable;
 
 import javax.inject.Inject;
-import javax.swing.plaf.nimbus.State;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.Observable;
 
 /**
- * Created by david on 10/29/2015.
+ * Presenter for the Loading screen.
+ *
+ * This is the screen where you can restore previously saved games.
  */
-public class LoadScreenPresenter implements Presenter, Validateable {
+public class LoadScreenPresenter implements Presenter {
     @Inject private GameState gameState;
     @Inject private Database database;
+    @Inject private Stage primaryStage;
+    @Inject private KeyHandler keyHandler;
 
     @FXML private TableView<GameState> gamesTableView;
 
@@ -34,11 +38,6 @@ public class LoadScreenPresenter implements Presenter, Validateable {
         games.add(new GameState());
         games.add(gameState);
         System.out.println(database);
-    }
-
-    @Override
-    public boolean isValid() {
-        return false;
     }
 
     public void load(ActionEvent actionEvent) {
@@ -63,12 +62,33 @@ public class LoadScreenPresenter implements Presenter, Validateable {
                     newPlayer.getNameProp().setValue(playersRs.getString("name"));
                     players[i] = newPlayer;
                 }
-
-
             }
         } catch (SQLException e) {
             System.out.println("Unable to load game");
         }
+    }
 
+    /**
+     * Starts a new game from a previous state.
+     *
+     * @param gameState the loaded game's GameState.
+     * @param turnManager the loaded game's TurnManager
+     * @param map the loaded game's Map
+     */
+    private void startGame(GameState gameState, TurnManager turnManager, Map map) {
+        // Disable land selection phase.
+        LandSelectionManager lsMan = new LandSelectionManager();
+        lsMan.getInLandSelectionPhaseProp().set(false);
+
+        Injector.setModelOrService(gameState.getClass(), gameState);
+        Injector.setModelOrService(turnManager.getClass(), turnManager);
+        Injector.setModelOrService(map.getClass(), map);
+        Injector.setModelOrService(lsMan.getClass(), lsMan);
+
+        new MainScreenView().getViewAsync(view -> {
+            Scene scene = new Scene(view);
+            scene.addEventHandler(EventType.ROOT, keyHandler::handle);
+            primaryStage.setScene(scene);
+        });
     }
 }
